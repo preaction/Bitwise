0b4cae7f811a4fb6b1cd37267c2330fb7b87fbd9
Doug Bell <doug@preaction.me>
2022-09-18 18:13:08 -0500

    add bitecs for entity-component-system
    This was easier than expected!

--- src/bitwise/Scene.ts
+++ src/bitwise/Scene.ts
@@ -11,8 +11,10 @@
  * may have corresponding Physics components on Entities.
  */
 import * as three from 'three';
+import * as bitecs from 'bitecs';
 import Game from './Game.ts';
-import OrthographicCamera from './component/OrthographicCamera.ts';
+import OrthographicCamera from './system/OrthographicCamera.ts';
+import Position from './system/Position.ts';
 import Tileset from './Tileset.ts';
 import { Tilemap, Tile } from './Tilemap.ts';
 
@@ -38,13 +40,28 @@ export default class Scene extends three.EventDispatcher {
   state:SceneState = SceneState.Stop;
   _scene:three.Scene = new three.Scene();
 
-  // XXX: This should be components in entities
-  camera:OrthographicCamera;
+  // world is the bitecs World object. Each scene has its own.
+  world:any;
+  // systems are the components available. This may need to be moved
+  // to the Game class, or the Game class be involved in defining which
+  // systems are available.
+  systems:any;
+  // entities are the bitecs entities in this scene.
+  entities:Number[];
+
+  cameraQuery:any;
 
   constructor( game:Game ) {
     super();
     this.game = game;
     game.addEventListener( "resize", (e:Object) => this.dispatchEvent("resize", e) );
+
+    this.world = bitecs.createWorld();
+    this.systems = {};
+    this.systems.Position = new Position( this );
+    this.systems.OrthographicCamera = new OrthographicCamera( this );
+
+    this.cameraQuery = bitecs.defineQuery([ this.systems.OrthographicCamera.component ]);
   }
 
   // start() should initialize the scene and get it ready to be
@@ -64,17 +81,21 @@ export default class Scene extends three.EventDispatcher {
     tilemap.setTile( new three.Vector2(0, 0), "dirt", 3 );
     this._scene.add( tilemap );
 
-    this.camera = new OrthographicCamera();
-    this.camera.addToScene( this );
+    const camera = bitecs.addEntity(this.world);
+    bitecs.addComponent(this.world, this.systems.Position.component, camera);
+    bitecs.addComponent(this.world, this.systems.OrthographicCamera.component, camera);
+    this.systems.Position.component.z[camera] = 4;
+    //this.systems.OrthographicCamera.component.zoom[camera] = 4;
 
     this.state = SceneState.Start;
   }
 
   update( timeMs:DOMHighResTimeStamp ) {
+    // XXX: Run through every system's update() method
+    this.systems.OrthographicCamera.update( timeMs );
   }
 
   render(renderer:three.Renderer) {
-    // XXX: Find all entities with cameras and render to them
-    this.camera.render( renderer, this._scene );
+    this.systems.OrthographicCamera.render( renderer );
   }
 }
---
+++ src/bitwise/system/OrthographicCamera.ts
@@ -0,0 +1,105 @@
+
+import * as three from 'three';
+import * as bitecs from 'bitecs';
+import Scene from './Scene.ts';
+
+export default class OrthographicCamera {
+  scene:Scene;
+  cameras:three.OrthographicCamera[] = [];
+  component:any;
+  position:any;
+
+  constructor( scene:Scene ) {
+    this.scene = scene;
+    this.component = bitecs.defineComponent( this.componentData );
+    this.position = scene.systems.Position;
+    this.query = bitecs.defineQuery([ this.position.component, this.component ]);
+    this.enterQuery = bitecs.enterQuery( this.query );
+    this.exitQuery = bitecs.exitQuery( this.query );
+
+    scene.addEventListener( "resize", (e:Object) => this.onResize(e) );
+  }
+
+  get world() {
+    return this.scene.world;
+  }
+
+  get componentData() {
+    return {
+      frustum: bitecs.Types.f32,
+      zoom: bitecs.Types.f32,
+      near: bitecs.Types.f32,
+      far: bitecs.Types.f32,
+    }
+  }
+
+  update( timeMilli:Number ) {
+    // enteredQuery for cameraQuery: Create Camera and add to Scene
+    const add = this.enterQuery(this.world);
+    for ( const eid of add ) {
+      this.add( eid ); 
+    }
+
+    // cameraQuery: Update camera properties if needed
+    const update = this.query(this.world);
+    for ( const eid of update ) {
+      // XXX
+    }
+
+    // exitedQuery for cameraQuery: Remove Camera from Scene
+    const remove = this.exitQuery(this.world);
+    for ( const eid of remove ) {
+      // XXX
+    }
+  }
+
+  add( eid:Number ) {
+    console.log( `Adding camera ${eid}` );
+    const { width, height } = this.scene.game;
+    // Point a camera at 0, 0
+    // Frustum size appears to work the same as zoom for an
+    // orthographic camera, which makes sense
+    const frustumSize = this.component.frustum[eid] || 2;
+    const camera = new three.OrthographicCamera(frustumSize * (width/-2), frustumSize * (width/2), frustumSize * (height/2), frustumSize * (height/-2), 0);
+    this.cameras[eid] = camera;
+    // Z-position determines which objects are rendered? Use z-position
+    // for layering?
+    camera.position.z = camera.far;
+    camera.zoom = this.component.zoom[eid] || 10;
+
+    this.scene._scene.add( camera );
+  }
+
+  remove( eid:Number ) {
+    this.scene._scene.remove( this.cameras[eid] );
+    this.cameras[eid] = null;
+  }
+
+  onResize(e:{width:Number, height:Number}) {
+    // Fix camera settings to maintain exact size/aspect
+    const { width, height } = e;
+    const update = this.query(this.world);
+    for ( const eid of update ) {
+      const frustumSize = this.component.frustum[eid];
+      const camera = this.cameras[eid];
+      camera.left = frustumSize * (width/-2);
+      camera.right = frustumSize * (width/2);
+      camera.top = frustumSize * (height/2);
+      camera.bottom = frustumSize * (height/-2);
+      camera.updateProjectionMatrix();
+    }
+  }
+
+  render(renderer:three.Renderer) {
+    // enteredQuery for cameraQuery: Create Camera and add to Scene
+    const add = this.enterQuery(this.world);
+    for ( const eid of add ) {
+      this.add( eid ); 
+    }
+
+    const cameras = this.query(this.world);
+    for ( const eid of cameras ) {
+      renderer.render( this.scene._scene, this.cameras[eid] );
+    }
+  }
+}
