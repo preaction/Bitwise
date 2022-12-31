a6b15f6e7d1ba86bf832e05f573086f3c21b6448
Doug Bell <doug@preaction.me>
2022-12-31 12:23:04 -0600

    update logo to better match macos roundings

--- extras/build-icon.sh
+++ extras/build-icon.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 convert -background transparent "icon.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "icon.ico"
-mkdir icon.iconset
+if [[ ! -d "icon.iconset" ]]; then
+  mkdir icon.iconset
+fi
 cp icon.png icon.iconset/
 cd icon.iconset
 convert -resize 16x16 ../icon.png icon_16x16.png
