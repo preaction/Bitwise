fdb2bddcee5b71450f2e3415bed19b6d47375978
Doug Bell <doug@preaction.me>
2022-12-31 10:54:23 -0600

    improve electron builder configuration

---
+++ extras/build-icon.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+convert -background transparent "icon.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "icon.ico"
+mkdir icon.iconset
+cp icon.png icon.iconset/
+cd icon.iconset
+convert -resize 16x16 ../icon.png icon_16x16.png
+convert -resize 32x32 ../icon.png icon_16x16@2x.png
+convert -resize 32x32 ../icon.png icon_32x32.png
+convert -resize 64x64 ../icon.png icon_32x32@2x.png
+convert -resize 128x128 ../icon.png icon_128x128.png
+convert -resize 256x256 ../icon.png icon_128x128@2x.png
+convert -resize 256x256 ../icon.png icon_256x256.png
+convert -resize 512x512 ../icon.png icon_256x256@2x.png
+convert -resize 512x512 ../icon.png icon_512x512.png
+convert -resize 1024x1024 ../icon.png icon_512x512@2x.png
+cd ..
+iconutil -c icns icon.iconset
