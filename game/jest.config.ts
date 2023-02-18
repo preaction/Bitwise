5557ecf5bc962acb5c2b4451d81af9f39d0ae887
Doug Bell <doug@preaction.me>
2023-02-18 13:39:20 -0600

    add jest configuration for automated testing

---
+++ game/jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!three/examples/jsm/.*)',
+  ],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+    'node_modules/three/examples/jsm/.+\\.js$': 'babel-jest',
+  },
+};
+export default config;
