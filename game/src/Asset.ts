674ec376235f65f9365ac29891c63fd66a8bb643
Doug Bell <doug@preaction.me>
2023-08-13 21:01:11 -0500

    add ref/deref of Assets
    This is the new way of linking to assets.

--- game/src/Asset.ts
+++ game/src/Asset.ts
@@ -1,11 +1,37 @@
 
 import Load from './Load.js';
 
+export type AssetProps = {
+  path: string,
+  [key: string]: any,
+};
+export type AssetRef = {
+  $asset: string,
+  path: string,
+  [key: string]: any,
+};
+type AssetConstructor<T extends Asset> = new ( load:Load, props:AssetProps ) => T;
+
 /**
  * Asset is the base class of a single game resource. Assets may be
  * actual (files and folders) or virtual (slices of an image).
  */
 export default class Asset {
+  static classes:{ [key:string]: AssetConstructor<any> } = {};
+  static register<T extends Asset>( cls:AssetConstructor<T> ) {
+    if ( Asset.classes[ cls.name ] && Asset.classes[ cls.name ] != cls ) {
+      throw `Another Asset named ${cls.name} already registered!`;
+    }
+    Asset.classes[cls.name] = cls;
+  }
+  static deref<T extends Asset>( load:Load, ref:AssetRef ):T {
+    const cons = Asset.classes[ ref.$asset ];
+    if ( !cons ) {
+      throw `Unknown asset type "${ref.$asset}"`;
+    }
+    return new cons( load, ref );
+  }
+
   /**
    * The loader that loaded this asset.
    */
@@ -21,12 +47,23 @@ export default class Asset {
    */
   children?:Asset[];
 
-  constructor( load:Load, path:string ) {
+  constructor( load:Load, props:string|AssetProps="" ) {
     if ( !load ) {
       throw new Error("Asset: Load object must be given to constructor");
     }
     this.load = load;
-    this.path = path;
+    if ( typeof props === 'string' ) {
+      this.path = props;
+    }
+    else {
+      this.path = props.path ?? "";
+    }
   }
 
+  ref():AssetRef {
+    return {
+      $asset: this.constructor.name,
+      path: this.path,
+    };
+  }
 }
--- game/test/Asset.test.ts
+++ game/test/Asset.test.ts
@@ -1,10 +1,70 @@
 
-import {describe, test, expect} from '@jest/globals';
-import Asset from '../src/Asset.js';
+import {describe, test, expect, beforeEach} from '@jest/globals';
+import Load from '../src/Load.js';
+import Asset, {AssetProps} from '../src/Asset.js';
+
+class MockAsset extends Asset {}
+const mockAssetCons = MockAsset;
 
 describe('Asset', () => {
   test('requires a Load object', () => {
     // @ts-ignore
     expect( () => new Asset(null, "") ).toThrow();
   } );
+
+  describe('register()', () => {
+    beforeEach( () => {
+      Asset.classes = {};
+    } );
+
+    test( 'register uses constructor name', () => {
+      Asset.register( MockAsset );
+      expect( Asset.classes ).toHaveProperty( MockAsset.name );
+      expect( Asset.classes[MockAsset.name] ).toBe( MockAsset );
+    });
+    test( 'cannot register different classes with same name', () => {
+      class MockAsset extends Asset {
+        constructor( load:Load, opt:AssetProps ) {
+          super(load, opt);
+        }
+      };
+      expect( () => Asset.register(mockAssetCons) ).not.toThrow();
+      expect( () => Asset.register( MockAsset ) ).toThrow(/already registered/);
+      expect( () => Asset.register(mockAssetCons) ).not.toThrow();
+    });
+  });
+
+  describe( 'ref()/deref()', () => {
+    let load:Load;
+    beforeEach( () => {
+      load = new Load();
+      Asset.classes = {};
+      Asset.register( MockAsset );
+    });
+
+    test('ref()', () => {
+      const path = "path/to/asset";
+      const asset = new MockAsset(load, { path });
+      const ref = asset.ref();
+      expect( ref.$asset ).toBe( MockAsset.name );
+      expect( ref.path ).toBe( path );
+    } );
+
+    test('deref()', () => {
+      const path = "path/to/asset";
+      const ref = {
+        $asset: MockAsset.name,
+        path,
+      };
+      const asset = Asset.deref( load, ref );
+      expect( asset ).toBeInstanceOf(MockAsset)
+      expect( asset.path ).toBe( path );
+    } );
+
+    test('roundtrip', () => {
+      const asset = new MockAsset(load, { path: "" });
+      const ref = asset.ref();
+      expect( Asset.deref( load, ref ) ).toEqual( asset );
+    });
+  });
 });
