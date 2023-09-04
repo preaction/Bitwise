ffb821736c99f5312e5b43f6c97a7e6f44c31677
Doug Bell <doug@preaction.me>
2023-09-04 14:51:19 -0500

    export Asset class

--- game/src/index.ts
+++ game/src/index.ts
@@ -2,6 +2,7 @@ import { default as Component } from './Component.js';
 import { default as Entity } from './Entity.js';
 import { default as Game } from './Game.js';
 import { default as Load } from './Load.js';
+import { default as Asset } from './Asset.js';
 import { Pointer } from './system/Input.js';
 import { default as Scene } from './Scene.js';
 import { default as System } from './System.js';
@@ -9,5 +10,5 @@ import * as event from './event/index.js';
 import * as system from './system/index.js';
 import * as component from './component/index.js';
 export {
-  Component, Entity, Game, Load, Pointer, Scene, System, event, system, component,
+  Component, Asset, Entity, Game, Load, Pointer, Scene, System, event, system, component,
 };
